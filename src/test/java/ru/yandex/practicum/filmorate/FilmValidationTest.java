c5fb68daa4852a68f6f12ff3b4d8a00888a420fc
LeChat <serion525@gmail.com>
2024-07-11 20:19:47 +0300

    refactor: Удалить старые тесты

--- src/test/java/ru/yandex/practicum/filmorate/FilmValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmValidationTest.java
@@ -1,31 +1,29 @@
 package ru.yandex.practicum.filmorate;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.controller.FilmController;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DisplayName("Проверить валидацию фильмов")
+@DisplayName("Проверить валидацию модели фильма")
 public class FilmValidationTest {
-    private FilmController filmController;
+    private Validator validator;
     private Film film;
 
     @BeforeEach
     void setUp() {
-        filmController = new FilmController();
-
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
         film = new Film();
         film.setId(1L);
         film.setName("Test");
@@ -34,191 +32,77 @@ public class FilmValidationTest {
         film.setDuration(90);
     }
 
-    @Nested
-    @DisplayName("Валидация при создании")
-    class FilmValidationWhenCreateTest {
-        @Test
-        @DisplayName("Создать корректный фильм")
-        void shouldCreateFilm() {
-            Film createdFilm = filmController.create(film);
-
-            List<Film> films = filmController.getAll();
-
-            assertNotNull(createdFilm, "Созданный фильм не вернулся");
-            assertNotNull(films, "Список фильмов не вернулся");
-            assertEquals(1, films.size(), "Фильм не создался в контроллере");
-            assertEquals(film, createdFilm, "Вернулся неккоректный фильм");
-            assertEquals(film, films.getFirst(), "Создался неккоректный фильм");
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при создании фильма с пустым названием")
-        void shouldExceptionWhenCreateFilmWithEmptyName() {
-            film.setName(null);
-
-            assertThrows(ValidationException.class, () -> filmController.create(film));
-
-            film.setName(" ");
-
-            assertThrows(ValidationException.class, () -> filmController.create(film));
-        }
-
-        @Test
-        @DisplayName("Создать фильм с описанием длиной в 200 символов")
-        void shouldCreateFilmWithDescriptionLength200() {
-            film.setDescription("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
-                    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
-                    "aaaaaaaaaaaaaa");
-
-            assertDoesNotThrow(() -> filmController.create(film));
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при создании фильма с описанием длинее 200 символов")
-        void shouldExceptionWhenCreateFilmWithDescriptionLengthOver200() {
-            film.setDescription("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
-                    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
-                    "aaaaaaaaaaaaaaa");
-
-            assertThrows(ValidationException.class, () -> filmController.create(film));
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при создании фильма с датой релиза раньше 28.12.1895")
-        void shouldExceptionWhenCreateFilmWithReleaseDateBeforeFilmCreation() {
-            film.setReleaseDate(LocalDate.of(1895, 12, 27));
-
-            assertThrows(ValidationException.class, () -> filmController.create(film));
-        }
-
-        @Test
-        @DisplayName("Создать фильм с с датой релиза 28.12.1895")
-        void shouldCreateFilmWithReleaseDateOfFilmCreation() {
-            film.setReleaseDate(LocalDate.of(1895, 12, 28));
-
-            assertDoesNotThrow(() -> filmController.create(film));
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при создании фильма с нулевой продолжительностью")
-        void shouldExceptionWhenCreateFilmWithZeroDuration() {
-            film.setDuration(0);
-
-            assertThrows(ValidationException.class, () -> filmController.create(film));
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при создании фильма с отрицательной продолжительностью")
-        void shouldExceptionWhenCreateFilmWithNegativeDuration() {
-            film.setDuration(-1);
-
-            assertThrows(ValidationException.class, () -> filmController.create(film));
-        }
+    @Test
+    @DisplayName("Пройти валидацию при корректных данных фильма")
+    void shouldSuccessValidationWhenValidFilmData() {
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertTrue(violations.isEmpty(), "Ошибка валидации при коректных данных фильма");
     }
 
-    @Nested
-    @DisplayName("Валидация при обновлении")
-    class FilmValidationWhenUpdateTest {
-        private Film newFilm;
+    @Test
+    @DisplayName("Провалить валидацию при некорректном имени")
+    void shouldFailValidationWhenInvalidName() {
+        film.setName(" ");
 
-        @BeforeEach
-        void setUp() {
-            filmController.create(film);
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertFalse(violations.isEmpty(), "Неккоректная валидация названия фильма");
+    }
 
-            newFilm = new Film();
-            newFilm.setId(1L);
-            newFilm.setName("new name");
-            newFilm.setDescription("new description");
-            newFilm.setReleaseDate(LocalDate.now().minusDays(2));
-            newFilm.setDuration(95);
-        }
+    @Test
+    @DisplayName("Пройти валидацию при описании длинной 200 символов")
+    void shouldSuccessValidationWhenDescriptionLengthEquals200() {
+        film.setDescription("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaa");
 
-        @Test
-        @DisplayName("Обновить фильм")
-        void shouldCreateFilm() {
-            Film updatedFilm = filmController.update(newFilm);
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertTrue(violations.isEmpty(), "Ошибка валидации при корректном описании фильма");
+    }
 
-            List<Film> films = filmController.getAll();
+    @Test
+    @DisplayName("Провалить валидацию при описании длинной больше 200 символов")
+    void shouldFailValidationWhenDescriptionLengthOver200() {
+        film.setDescription("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaa");
 
-            assertNotNull(updatedFilm, "Обновлённый фильм не вернулся");
-            assertEquals(newFilm, updatedFilm, "Вернулся неккоректный фильм");
-            assertEquals(newFilm, films.getFirst(), "Фильм не обновился");
-        }
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertFalse(violations.isEmpty(), "Неккоректная валидация описания фильма");
+    }
 
-        @Test
-        @DisplayName("Выбросить исключение при пустом ID")
-        void shouldExceptionWhenUpdateFilmWithoutId() {
-            newFilm.setId(null);
+    @Test
+    @DisplayName("Пройти валидацию при дате релиза в день рождения кино")
+    void shouldSuccessValidationWhenReleaseDateIsFilmBirthday() {
+        film.setReleaseDate(LocalDate.of(1895, 12, 28));
 
-            assertThrows(ValidationException.class, () -> filmController.update(newFilm));
-        }
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertTrue(violations.isEmpty(), "Ошибка валидации при корректной дате релиза фильма");
+    }
 
-        @Test
-        @DisplayName("Выбросить исключение при неккоректном ID")
-        void shouldExceptionWhenUpdateFilmWithIncorrectId() {
-            newFilm.setId(2L);
+    @Test
+    @DisplayName("Провалить валидацию при дате релиза раньше дня рождения кино")
+    void shouldFailValidationWhenReleaseDateIsBeforeFilmBirthday() {
+        film.setReleaseDate(LocalDate.of(1895, 12, 27));
 
-            assertThrows(NotFoundException.class, () -> filmController.update(newFilm));
-        }
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertFalse(violations.isEmpty(), "Неккоректная валидация даты релиза фильма");
+    }
 
-        @Test
-        @DisplayName("Выбросить исключение при обновлении фильма с пустым названием")
-        void shouldExceptionWhenUpdateFilmWithEmptyName() {
-            newFilm.setName(" ");
+    @Test
+    @DisplayName("Провалить валидацию при нулевой продолжительности")
+    void shouldFailValidationWhenZeroDuration() {
+        film.setDuration(0);
 
-            assertThrows(ValidationException.class, () -> filmController.update(newFilm));
-        }
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertFalse(violations.isEmpty(), "Неккоректная валидация продолжительности фильма");
+    }
 
-        @Test
-        @DisplayName("Обновить фильм с описанием длиной в 200 символов")
-        void shouldUpdateFilmWithDescriptionLength200() {
-            newFilm.setDescription("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
-                    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
-                    "aaaaaaaaaaaaaa");
+    @Test
+    @DisplayName("Провалить валидацию при отрицательной продолжительности")
+    void shouldFailValidationWhenNegativeDuration() {
+        film.setDuration(-1);
 
-            assertDoesNotThrow(() -> filmController.update(newFilm));
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при обновлении фильма с описанием длинее 200 символов")
-        void shouldExceptionWhenUpdateFilmWithDescriptionLengthOver200() {
-            newFilm.setDescription("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
-                    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
-                    "aaaaaaaaaaaaaaa");
-
-            assertThrows(ValidationException.class, () -> filmController.update(newFilm));
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при обновлении фильма с датой релиза раньше 28.12.1895")
-        void shouldExceptionWhenUpdateFilmWithReleaseDateBeforeFilmCreation() {
-            newFilm.setReleaseDate(LocalDate.of(1895, 12, 27));
-
-            assertThrows(ValidationException.class, () -> filmController.update(newFilm));
-        }
-
-        @Test
-        @DisplayName("Обновить фильм с с датой релиза 28.12.1895")
-        void shouldUpdateFilmWithReleaseDateOfFilmCreation() {
-            newFilm.setReleaseDate(LocalDate.of(1895, 12, 28));
-
-            assertDoesNotThrow(() -> filmController.update(newFilm));
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при обновлении фильма с нулевой продолжительностью")
-        void shouldExceptionWhenUpdateFilmWithZeroDuration() {
-            newFilm.setDuration(0);
-
-            assertThrows(ValidationException.class, () -> filmController.update(newFilm));
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при обновлении фильма с отрицательной продолжительностью")
-        void shouldExceptionWhenUpdateFilmWithNegativeDuration() {
-            newFilm.setDuration(-1);
-
-            assertThrows(ValidationException.class, () -> filmController.update(newFilm));
-        }
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertFalse(violations.isEmpty(), "Неккоректная валидация продолжительности фильма");
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/UserValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserValidationTest.java
@@ -1,30 +1,29 @@
 package ru.yandex.practicum.filmorate;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.controller.UserController;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DisplayName("Проверить валидацию пользователей")
+@DisplayName("Проверить валидацию модели пользователя")
 public class UserValidationTest {
-    private UserController userController;
+    private Validator validator;
     private User user;
 
     @BeforeEach
     void setUp() {
-        userController = new UserController();
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
         user = new User();
         user.setId(1L);
         user.setEmail("email@email.com");
@@ -32,162 +31,55 @@ public class UserValidationTest {
         user.setBirthday(LocalDate.now().minusDays(1));
     }
 
-    @Nested
-    @DisplayName("Валидация при создании")
-    class UserValidationWhenCreateTest {
-        @Test
-        @DisplayName("Создать корректного пользователя")
-        void shouldCreateUser() {
-            User createdUser = userController.create(user);
-
-            List<User> users = userController.getAll();
-
-            assertNotNull(createdUser, "Созданный пользователь не вернулся");
-            assertNotNull(users, "Список пользователей не вернулся");
-            assertEquals(1, users.size(), "Пользователь не создался в контроллере");
-            assertEquals(user, createdUser, "Вернулся неккоректный пользователь");
-            assertEquals(user, users.getFirst(), "Создался неккоректный пользователь");
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при создании пользователя с неккоректной электронной почтой")
-        void shouldThrowExceptionWhenCreateUserWithIncorrectEmail() {
-            user.setEmail(null);
-
-            assertThrows(ValidationException.class, () -> userController.create(user));
-
-            user.setEmail(" ");
-
-            assertThrows(ValidationException.class, () -> userController.create(user));
-
-            user.setEmail("email");
-
-            assertThrows(ValidationException.class, () -> userController.create(user));
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при создании пользователя с пустым логином")
-        void shouldThrowExceptionWhenCreateUserWithEmptyLogin() {
-            user.setLogin(null);
-
-            assertThrows(ValidationException.class, () -> userController.create(user));
-
-            user.setLogin(" ");
-
-            assertThrows(ValidationException.class, () -> userController.create(user));
-        }
-
-        @Test
-        @DisplayName("Использовать логин в качестве имени при создании пользователя с пустым именем")
-        void shouldUseLoginInNameWhenNameIsEmpty() {
-            User createdUser = userController.create(user);
-
-            assertEquals(createdUser.getLogin(), createdUser.getName(), "Имя пользователя не заменилось на логин");
-        }
-
-        @Test
-        @DisplayName("Создать пользователя с датой рождения в настоящем")
-        void shouldCreateUserWithPresentDateOfBirth() {
-            user.setBirthday(LocalDate.now());
-
-            assertDoesNotThrow(() -> userController.create(user));
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при создании пользователя с датой рождения в будущем")
-        void shouldThrowExceptionWhenCreateUserWithFutureDateOfBirth() {
-            user.setBirthday(LocalDate.now().plusDays(1));
-
-            assertThrows(ValidationException.class, () -> userController.create(user));
-        }
+    @Test
+    @DisplayName("Пройти валидацию при корректных данных пользователя")
+    void shouldSuccessValidationWhenValidFilmData() {
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertTrue(violations.isEmpty(), "Ошибка валидации при корректных данных пользователя");
     }
 
-    @Nested
-    @DisplayName("Валидация при обновлении")
-    class UserValidationWhenUpdateTest {
-        private User newUser;
+    @Test
+    @DisplayName("Провалить валидацию при пустой электронной почте")
+    void shouldFailValidationWhenEmptyEmail() {
+        user.setEmail(" ");
 
-        @BeforeEach
-        void setUp() {
-            userController.create(user);
-            newUser = new User();
-            newUser.setId(1L);
-            newUser.setEmail("newemail@email.com");
-            newUser.setLogin("newlogin");
-            newUser.setBirthday(LocalDate.now().minusDays(2));
-        }
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertFalse(violations.isEmpty(), "Некорректная валидация при пустой электронной почте");
+    }
 
-        @Test
-        @DisplayName("Обновить корректного пользователя")
-        void shouldUpdateUser() {
-            userController.update(newUser);
+    @Test
+    @DisplayName("Провалить валидацию при некорректной электронной почте")
+    void shouldFailValidationWhenIncorrectEmail() {
+        user.setEmail("@test");
 
-            List<User> users = userController.getAll();
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertFalse(violations.isEmpty(), "Некорректная валидация при некорректной электронной почте");
+    }
 
-            newUser.setName(newUser.getLogin());
-            assertEquals(newUser, users.getFirst(), "Пользователь не обновился");
-        }
+    @Test
+    @DisplayName("Провалить валидацию при пустом логине")
+    void shouldFailValidationWhenEmptyLogin() {
+        user.setLogin(" ");
 
-        @Test
-        @DisplayName("Выбросить исключение при пустом ID")
-        void shouldExceptionWhenUpdateUserWithoutId() {
-            newUser.setId(null);
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertFalse(violations.isEmpty(), "Некорректная валидация при пустом логине");
+    }
 
-            assertThrows(ValidationException.class, () -> userController.update(newUser));
-        }
+    @Test
+    @DisplayName("Пройти валидацию при дате рождения в настоящем")
+    void shouldSuccessValidationWhenPresentBirthDay() {
+        user.setBirthday(LocalDate.now());
 
-        @Test
-        @DisplayName("Выбросить исключение при неккоректном ID")
-        void shouldExceptionWhenUpdateUserWithIncorrectId() {
-            newUser.setId(2L);
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertTrue(violations.isEmpty(), "Ошибка валидации при корректной дате рождения");
+    }
 
-            assertThrows(NotFoundException.class, () -> userController.update(newUser));
-        }
+    @Test
+    @DisplayName("Провалить валидацию при дате рождения в будущем")
+    void shouldFailValidationWhenFutureBirthDay() {
+        user.setBirthday(LocalDate.now().plusDays(1));
 
-        @Test
-        @DisplayName("Выбросить исключение при обновлении пользователя с неккоректной электронной почтой")
-        void shouldThrowExceptionWhenUpdateUserWithIncorrectEmail() {
-            newUser.setEmail(" ");
-
-            assertThrows(ValidationException.class, () -> userController.update(newUser));
-
-            newUser.setEmail("email");
-
-            assertThrows(ValidationException.class, () -> userController.update(newUser));
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при обновлении пользователя с пустым логином")
-        void shouldThrowExceptionWhenUpdateUserWithEmptyLogin() {
-            newUser.setLogin(" ");
-
-            assertThrows(ValidationException.class, () -> userController.update(newUser));
-        }
-
-        @Test
-        @DisplayName("Использовать логин в качестве имени при обновлении пользователя с логином в качестве имени")
-        void shouldUseLoginInNameWhenNameIsEmpty() {
-            user.setName(user.getLogin());
-            userController.update(newUser);
-            User updatedUser = userController.getAll().getFirst();
-
-            assertEquals(updatedUser.getLogin(), updatedUser.getName(), "Имя пользователя не заменилось на логин");
-        }
-
-        @Test
-        @DisplayName("Обновить пользователя с датой рождения в настоящем")
-        void shouldUpdateUserWithPresentDateOfBirth() {
-            newUser.setBirthday(LocalDate.now());
-
-            assertDoesNotThrow(() -> userController.update(newUser));
-        }
-
-        @Test
-        @DisplayName("Выбросить исключение при обновлении пользователя с датой рождения в будущем")
-        void shouldThrowExceptionWhenUpdateUserWithFutureDateOfBirth() {
-            newUser.setBirthday(LocalDate.now().plusDays(1));
-
-            assertThrows(ValidationException.class, () -> userController.update(newUser));
-        }
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        assertFalse(violations.isEmpty(), "Некорректная валидация при дате рождения в будущем");
     }
 }
