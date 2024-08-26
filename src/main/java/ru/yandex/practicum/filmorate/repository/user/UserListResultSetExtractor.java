c2fe2fb5eb68de9a8e24ae7f43457e1a6f838908
LeChat <serion525@gmail.com>
2024-08-26 17:50:51 +0300

    refactor: Вынести маппинг пользователя в абстрактный класс

--- src/main/java/ru/yandex/practicum/filmorate/repository/user/UserListResultSetExtractor.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/user/UserListResultSetExtractor.java
@@ -5,31 +5,19 @@ import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class UserListResultSetExtractor implements ResultSetExtractor<List<User>> {
+public class UserListResultSetExtractor extends AbstractUserResultSetExtractor implements ResultSetExtractor<List<User>> {
     @Override
     public List<User> extractData(ResultSet resultSet) throws SQLException, DataAccessException {
         List<User> users = new ArrayList<>();
 
         while (resultSet.next()) {
-            User user = new User();
-            user.setId(resultSet.getLong("user_id"));
-            user.setLogin(resultSet.getString("login"));
-            user.setEmail(resultSet.getString("email"));
-            user.setName(resultSet.getString("name"));
-
-            Date birthday = resultSet.getDate("birthday");
-            if (birthday != null) {
-                user.setBirthday(birthday.toLocalDate());
-            }
-
-            users.add(user);
+            users.add(mapUser(resultSet));
         }
         return users;
     }
--- src/main/java/ru/yandex/practicum/filmorate/repository/user/UserResultSetExtractor.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/user/UserResultSetExtractor.java
@@ -5,26 +5,14 @@ import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @Component
-public class UserResultSetExtractor implements ResultSetExtractor<User> {
+public class UserResultSetExtractor extends AbstractUserResultSetExtractor implements ResultSetExtractor<User> {
     @Override
     public User extractData(ResultSet resultSet) throws SQLException, DataAccessException {
         resultSet.next();
-        User user = new User();
-        user.setId(resultSet.getLong("user_id"));
-        user.setLogin(resultSet.getString("login"));
-        user.setEmail(resultSet.getString("email"));
-        user.setName(resultSet.getString("name"));
-
-        Date birthday = resultSet.getDate("birthday");
-        if (birthday != null) {
-            user.setBirthday(birthday.toLocalDate());
-        }
-
-        return user;
+        return mapUser(resultSet);
     }
 }
