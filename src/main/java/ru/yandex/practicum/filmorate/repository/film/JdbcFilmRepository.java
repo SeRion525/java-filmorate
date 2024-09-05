feff273d95b45d3c13fae54ae29e3191f1370bc9
Ksenia <kseniiabelka05@mail.ru>
2024-09-05 20:09:55 +0300

    Удаление фильмов и пользователей исправления

--- src/main/java/ru/yandex/practicum/filmorate/repository/film/JdbcFilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/film/JdbcFilmRepository.java
@@ -16,7 +16,14 @@ import java.util.Set;
 @Repository
 public class JdbcFilmRepository extends JdbcBaseRepository<Film> implements FilmRepository {
 
-    private static final String DELETE_FILM_QUERY = "DELETE FROM films WHERE filmId = :filmId";
+
+    private static final String DELETE_FILM_QUERY = """
+            DELETE FROM films WHERE film_id = :filmId;
+            """;
+
+    private static final String DELETE_FILM_GENRES_BY_FILM_ID_QUERY = """
+            DELETE FROM films_genres WHERE film_id = :filmId;
+            """;
 
     private static final String GET_ALL_QUERY = """
             SELECT films.*, mpa.name AS mpa_name, genres.genre_id, genres.name AS genre_name FROM films
@@ -48,8 +55,6 @@ public class JdbcFilmRepository extends JdbcBaseRepository<Film> implements Film
             SET name = :name, description = :desc, release_date = :date, duration = :dur, mpa_id = :mpa
             WHERE film_id = :filmId
             """;
-
-    private static final String DELETE_FILM_GENRES_BY_FILM_ID_QUERY = "DELETE FROM films_genres WHERE film_id = :filmId";
 
     private static final String ADD_LIKE_QUERY = """
             MERGE INTO likes AS t
@@ -173,6 +178,8 @@ public class JdbcFilmRepository extends JdbcBaseRepository<Film> implements Film
 
     @Override
     public void delete(long filmId) {
+        jdbc.update(DELETE_FILM_GENRES_BY_FILM_ID_QUERY, new MapSqlParameterSource("filmId", filmId));
         jdbc.update(DELETE_FILM_QUERY, new MapSqlParameterSource("filmId", filmId));
     }
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/BaseFilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/BaseFilmService.java
@@ -27,6 +27,7 @@ public class BaseFilmService implements FilmService {
 
 
     public static final String NOT_FOUND_FILM = "Не найден фильм с ID = ";
+
     private final FilmRepository filmRepository;
     private final UserRepository userRepository;
     private final MpaRepository mpaRepository;
