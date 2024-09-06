2649decb131cd574866a3a88d23327858ae1742d
LeChat <serion525@gmail.com>
2024-09-06 21:23:04 +0300

    feature: Добавить контроллер и сервис отзывов

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.filmorate.service;
+
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.util.List;
+
+public interface ReviewService {
+    Review createReview(Review review);
+
+    Review updateReview(Review newReview);
+
+    void deleteReview(long reviewId);
+
+    Review getReviewById(long reviewId);
+
+    List<Review> getReviewsByFilmId(Long filmId, int count);
+
+    void addLike(long reviewId, long userId);
+
+    void addDislike(long reviewId, long userId);
+
+    void removeLike(long reviewId, long userId);
+
+    void removeDislike(long reviewId, long userId);
+}
